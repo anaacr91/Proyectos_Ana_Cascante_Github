e1f70cdf8de1cc5c33354a0b1fd918092bae7c68
Ana Cascante <162157067+anaacr91@users.noreply.github.com>
2024-09-23 17:58:29 +0200

    Delete JS, TS, ANGULAR/curso testing con cypress directory

--- JS, TS, ANGULAR/curso testing con cypress/curs cypress/cypress/cypress/integration/examples/BDD/ecommerce/beforeEach.js
+++
@@ -1,5 +0,0 @@
-beforeEach(()=>{
-    cy.fixture('example').then(function(data){  //mocha does'nt support arrow function
-        this.data = data  //this.data is a global variable for the whole class
-    })
-})
--- JS, TS, ANGULAR/curso testing con cypress/curs cypress/cypress/cypress/integration/pageObjects/ProductsPage.js
+++
@@ -1,9 +0,0 @@
-class ProductPage{
-
-    getCheckOutButton(){
-        return cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link')
-    }
-
-}
-
-export default ProductPage; //export the class to be used in other files
