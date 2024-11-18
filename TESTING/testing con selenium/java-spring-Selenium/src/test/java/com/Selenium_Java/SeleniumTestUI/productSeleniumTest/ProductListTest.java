0d10d5e072c4e806d26fbd0c54b25cc1ac532113
anaacr91 <anacascanterodriguez@gmail.com>
2024-11-18 13:30:04 +0100

    test UI

--- TESTING/testing con selenium/java-spring-Selenium/src/test/java/com/Selenium_Java/SeleniumTestUI/productSeleniumTest/ProductListTest.java
+++ TESTING/testing con selenium/java-spring-Selenium/src/test/java/com/Selenium_Java/SeleniumTestUI/productSeleniumTest/ProductListTest.java
@@ -1,17 +1,20 @@
 package com.Selenium_Java.SeleniumTestUI.productSeleniumTest;
 
 import com.Selenium_Java.repository.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /*
 Test funcional/UI de selenium del listado de productos product-list.html
 Requiere la dependencia selenium-java
@@ -29,50 +32,65 @@ public class ProductListTest {
     @Autowired
     private ProductRepository productRepository;
 
-    @BeforeEach
-    void setUp() {
+    WebDriver driver;
 
+    @BeforeEach// setUp se ejecuta antes de cada test, crea un driver para navegar a la url de test
+    void setUp() {
+        driver = new ChromeDriver();//crear instancia de ChromeDriver
+        //declars objeto driver, navegar por la pantalla y hacer aserciones q
+        // entrar a la pantalla productos
+        //webdriver es una clase que representa el navegador
+        driver.get("http://localhost:8080/productos");//abrir navegador en la dirección especificada
+    }
+    @AfterEach
+        // Se ejecuta al final de cada test
+    void tearDown() {
+        driver.quit();//cerrar navegador para que no quede el navegador abierto
     }
 
     @Test
     @DisplayName("Comprobar etiqueta <title>")
     void title() {
-        WebDriver driver = new ChromeDriver();//crear instancia de ChromeDriver
-        //declars objeto driver, navegar por la pantalla y hacer aserciones q
-        // entrar a la pantalla productos
-        //webdriver es una clase que representa el navegador
-        driver.get("http://localhost:8080/productos");//abrir navegador en la dirección especificada
-
         // comprobar título
         String title = driver.getTitle();//obtener título de la página web
         System.out.println(title);
         assertEquals("Product List", title);
-        ;//comparar si es igual a Productos (título de la página)
-
-        driver.quit();//cerrar navegador para que no quede el navegador abierto
+        //comparar si es igual a Productos (título de la página)
     }
 
     @Test
     @DisplayName("Comprobar la etiqueta <h1>")
     void h1() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/productos");
         WebElement h1 = driver.findElement(By.tagName("h1"));
         //by es una clase que tiene metodos para buscar elementos por nombre, id, clase, etc
         //webelement es una clase que representa un elemento de la página web
         assertEquals("Lista de productos", h1.getText());
-        driver.quit();
     }
 
     @Test
     @DisplayName("Comprobar que existe el enlace de Crear nuevo producto y su texto")
     void buttonCreateProduct() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/productos");
         WebElement createButton = driver.findElement(By.id("btnCreateProduct"));
         assertEquals("Crear nuevo producto", createButton.getText());
         createButton.click(); // pulsa botón crear nuevo producto
         assertEquals("http://localhost:8080/productos/crear", driver.getCurrentUrl());
-        driver.quit();
     }
+    //comprobar cuando la tabla no tiene elementos
+    @Test
+    @DisplayName("comprobar que la tabla vacía, no tiene elementos")
+    void tableEmpty() {
+        //comprobar que el mensaje de no hay productos es igual a productsEmpty
+        WebElement noProductsMessage = driver.findElement(By.id("productsEmpty"));
+        //comprobar que el texto del mensaje es igual a no hay productos
+        assertEquals("No hay productos", noProductsMessage.getText());
+        //comprobar que la tabla de productos no está visible
+        //WebElement productsTable = driver.findElement(By.id("productList"));
+        //lanza una excepcion si no encuentra el elemento productList
+        assertThrows(
+                NoSuchElementException.class,
+                () -> driver.findElement(By.id("productList"))
+        );
+    }
+
+
 }
